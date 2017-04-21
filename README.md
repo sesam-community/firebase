fc0f8e720a33d49c7bb69fb10fcf5f28b6f590e9
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2017-04-21 13:17:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# sesam-firebase
+Reads and writes streams of entities to Firebase Realtime DB.
+
+Implemented using the Firebase NodeJS Admin SDK.
+
+Sinks and sources defines a path to where in the Firebase DB tree the data should be read or written to.
+
+## sources
+The source will create one entity per key, where the key will be the "_id" property of the entities.
+```json
+{
+  "0001": {
+    "name": "Foo"
+  }
+}
+```
+will become:
+```json
+[
+  {
+    "_id": "0001",
+    "name": "Foo"
+  }
+]
+```
+
+In order to support since, you need to configure a path you want to use for since values. You will need an index on this path. Add this in the rules section
+of your Firebase DB like this (in this case the source path is my-users, and marker path is 'last-modified'):
+```json
+{
+  "rules": {
+    "my-users": {
+      ".indexOn": ["last-modified"]
+    }
+  }
+}
+```
+
+The path is passed as url parameter to the source. See example.
+
+To make sure that Sesam never misses any entities when using since, we recommend that you add the since value using Firebase.ServerValue.TIMESTAMP.
+
+## sinks
+The sink will PUT the entity under a key specified by the "_id" property of the entities it receives. The sink will DELETE entities that has the "_deleted" property
+set to true.
+
+If you want to create a server side value inside Firebase when writing entities, you can add a property on your entities like this:
+```json
+{
+  "_id": "foo",
+  "last-modified": {
+    ".sv": "timestamp"
+  }
+}
+```
+
+(see https://firebase.google.com/docs/reference/rest/database/#section-param-query for more info)
+
+An example of system config: 
+
+```json
+[{
+  "_id": "my-firebase",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "KEYFILE": {
+          "type": "service_account",
+          "..": ".."
+      },
+      "PROJECT_ID": "my-firebase-instance"
+    },
+    "image": "sesamcommunity/sesam-firebase-db:latest",
+    "port": 5000
+  }
+},
+{
+  "_id": "my-foo-reader",
+  "type": "pipe",
+  "source": {
+    "type": "json",
+    "system": "my-firebase",
+    "supports_since": true,
+    "url": "/foo?since_path=last-modified"
+  }
+}]
+```
+
+The keyfile is obtained by creating a key for the service account in the Firebase console (request a JSON keyfile).
---
+++ service/server.js
@@ -0,0 +1,124 @@
+// Load the http module to create an http server.
+var http = require('http');
+var Router = require('node-simple-router');
+var url = require('url');
+var admin = require("firebase-admin");
+
+var serviceAccount = JSON.parse(process.env.KEYFILE);
+var projectId = process.env.PROJECT_ID;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://" + projectId + ".firebaseio.com"
+});
+
+var db = admin.database();
+
+// we need to encode and decode the type of the value we use as since marker
+var encode = function (since) {
+  if (typeof since === "number") {
+    return "n:" + since;
+  } else {
+    return "s:" + since;
+  }
+};
+
+var decode = function (since) {
+  if (since == undefined) {
+    return since;
+  }
+  if (since.startsWith("n:")) {
+    return parseFloat(since.substr(2));
+  } else {
+    return since.substr(2);
+  }
+};
+
+var router = Router();
+
+router.get("/:path", function (request, response) {
+  var query = url.parse(request.url, true).query;
+  var updatedPath = query.since_path;
+  var updated = decode(query.since);
+  var path = request.params.path;
+  var ref = db.ref(path);
+  if (updatedPath && updated) {
+    ref = ref.orderByChild(updatedPath).startAt(updated);
+  }
+  ref.once("value", function (snapshot) {
+    var result = [];
+    var entities = snapshot.forEach(function (data) {
+      var val = data.val();
+      var since = val[updatedPath];
+      // drop entities that has exact since value, startAt is fixed to greater than or equal
+      if (updatedPath && updated == since) {
+        return;
+      }
+      var entity = { "_id": data.key };
+      if (since) {
+        entity["_updated"] = encode(since);
+      }
+      Object.assign(entity, val);
+      result.push(entity);
+    });
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(result));
+  });
+});
+
+router.post("/:path", function (request, response) {
+  var path = request.params.path;
+  var ref = db.ref(path);
+  var entities = request.post;
+  // might get one entity or a list
+  var entities = [].concat(entities);
+
+  var notCompleted = entities.length;
+  var errors = [];
+  var completionHandler = function (entity, error) {
+    notCompleted--;
+    if (error) {
+      errors.push(error);
+      console.error("Failed to handle", entity, error);
+    }
+    if (notCompleted == 0) {
+      if (errors.length > 0) {
+        response.writeHead(500, { "Content-Type": "plain/text" });
+        response.end("Oops, something went wrong, check server log!");
+      } else {
+        response.writeHead(200, { "Content-Type": "plain/text" });
+        response.end("Done!");
+      }
+    } else {
+      // nice to get some progress when handling huge number of entities
+      if (notCompleted % 10000 == 0) {
+        console.log("Waiting for " + notCompleted + " to finish");
+      }
+    }
+  };
+
+  entities.forEach(function (entity) {
+    var id = entity["_id"];
+    var filtered;
+    if (entity["_deleted"] && entity["_deleted"] === true) {
+      filtered = null;
+    } else {
+      filtered = {};
+      Object.keys(entity).filter(function (k) {
+        return !k.startsWith("_")
+      }).forEach(function (k) {
+        filtered[k] = entity[k];
+      });
+    }
+    ref.child(id).set(filtered, function(error) { completionHandler(entity, error) });
+  });
+});
+
+// Configure our HTTP server to use router function
+var server = http.createServer(router);
+
+// Listen on port 5000, IP defaults to 127.0.0.1
+server.listen(5000, "0.0.0.0");
+
+// Put a friendly message on the terminal
+console.log("Server running at http://0.0.0.0:5000/");
